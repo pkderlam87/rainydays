31165fcf46295ca359d9f9b31f344bef370e0776
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-05-21 18:06:40 +0200

    Patterns of photos

--- js/products/stok.js
+++ js/products/stok.js
@@ -1,38 +1,41 @@
-const productsStok = {
-    type1: {
-        model: "waterproof",
+const productsStok = [
+    {
+        type: "waterproof",
         price: 399,
         units: 10,
         photo: `<img src="images/raincoat-1245688_1920.jpg">`,
     },
-    type2: {
-        model: "waterproof",
+    {
+        type: "waterproof",
         price: 399,
         units: 10,
         photo: `<img src="images/karsten-winegeart-jVT8vo04UT0-unsplash.jpg">`,
     },
-    type3: {
-        model: "skiSnowboard",
+    {
+        type: "skiSnowboard",
         price: 599,
         units: 5,
         photo: `<img src="images/simon-berger-PcVqr8v4XaA-unsplash.jpg">`,
     },
-    type4: {
-        model: "windbreaker",
+    {
+        type: "windbreaker",
         price: 299,
         units: 15,
         photo: `<img src="images/joshua-earle-ICE__bo2Vws-unsplash.jpg">`,
     },
-    type5: {
-        model: "accessories",
+    {
+        type: "accessories",
         price: 99,
         units: 20,
         photo: `<img src="images/roberto-martinez-WBgPMst9toY-unsplash.jpg">`,
     },
-}
+];
 console.log(productsStok);
 const products_collections = document.querySelector(".products_collections");
-products_collections.innerHTML = `<div class="productsCards">${productsStok.type1}</div>`;
-//for (let i = 0; i < productsStok.length; i++) {
 
-//}
+for (let i = 0; i < productsStok.length; i++) {
+    products_collections.innerHTML += `<div class="productsCards">
+    ${productsStok[i].photo}
+    ${productsStok[i].type}
+    ${productsStok[i].price}</div>`;
+}
