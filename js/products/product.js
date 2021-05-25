731503d498e312fb8da979f4af08e2ad8283152c
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-05-25 23:14:12 +0200

    Adjust header on desktop version - best sellers on products page

--- js/products/product.js
+++ js/products/product.js
@@ -1,8 +1,17 @@
 const products_collections = document.querySelector(".products_collections");
-
+const bestSeller = document.querySelector(".bestSeller");
 for (let i = 0; i < productsStok.length; i++) {
     products_collections.innerHTML += `<a href = "/products/specific_product.html"> <div class="productsCards">
     ${productsStok[i].photo}
     <h4>${productsStok[i].type}</h4>
     <h4>${productsStok[i].price}, - Nok</h4></div></a>`;
+}
+//Best Sellers section
+for (let i = 0; i < productsStok.length; i++) {
+    if ((productsStok[i].bestseller) === true) {
+        bestSeller.innerHTML += `<a href = "/products/specific_product.html"> <div class="productsCards">
+    ${productsStok[i].photo}
+    <h4>${productsStok[i].type}</h4>
+    <h4>${productsStok[i].price}, - Nok</h4></div></a>`;
+    }
 }
